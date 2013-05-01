90378868c790cb6f0b9fb91600b8905a3e0c1f64
Jack Moffitt <jack@metajack.im>
2013-05-01 12:13:05 -0600

    Add shared GL contexts to Skia.
    This adds supprot for the mac. Linux and windows support may require changes
    to the the class definition.
    
    Add native shared gl contexts.

---
+++ include/gpu/gl/SkNativeSharedGLContext.h
@@ -0,0 +1,98 @@
+
+/*
+ * Copyright 2013 The Servo Project Developers
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef SkNativeSharedGLContext_DEFINED
+#define SkNativeSharedGLContext_DEFINED
+
+#include "SkGLContextHelper.h"
+#include "GrContext.h"
+
+#if defined(SK_BUILD_FOR_MAC)
+    #include <OpenGL/OpenGL.h>
+#elif defined(SK_BUILD_FOR_ANDROID) || defined(SK_BUILD_FOR_NACL)
+    #include <GLES2/gl2.h>
+    #include <EGL/egl.h>
+#elif defined(SK_BUILD_FOR_UNIX)
+    #include <X11/Xlib.h>
+    #include <GL/glx.h>
+#elif defined(SK_BUILD_FOR_WIN32)
+    #include <Windows.h>
+    #include <GL/GL.h>
+#endif
+
+#if defined(SK_BUILD_FOR_MAC)
+typedef CGLContextObj GrGLSharedContext;
+#elif defined(SK_BUILD_FOR_ANDROID)
+typedef EGLContext GrGLSharedContext;
+#elif defined(SK_BUILD_FOR_UNIX)
+typedef GLXContext GrGLSharedContext;
+#else
+#error "No shared contexts on this platform."
+#endif
+
+class SkNativeSharedGLContext : public SkRefCnt {
+public:
+    explicit SkNativeSharedGLContext(GrGLSharedContext sharedContext);
+    virtual ~SkNativeSharedGLContext();
+
+    virtual bool init(const int width, const int height);
+    virtual unsigned int getFBOID() const { return fFBO; }
+    virtual unsigned int getTextureID() const { return fTextureID; }
+    virtual const GrGLInterface *gl() const { return fGL; }
+    virtual GrContext *getGrContext();
+    virtual void makeCurrent() const;
+    virtual void flush() const;
+
+    virtual bool hasExtension(const char* extensionName) const {
+        GrAssert(NULL != fGL);
+        return fExtensions.has(extensionName);
+    }
+
+    static GrGLSharedContext GetCurrent() {
+        #if defined(SK_BUILD_FOR_MAC)
+        return CGLGetCurrentContext();
+        #elif defined(SK_BUILD_FOR_ANDROID) || defined(SK_BUILD_FOR_NACL)
+        #elif defined(SK_BUILD_FOR_UNIX)
+        #endif
+    }
+
+protected:
+    virtual const GrGLInterface *createGLContext();
+    virtual void destroyGLContext();
+
+private:
+#if defined(SK_BUILD_FOR_MAC)
+    CGLContextObj fContext;
+    CGLContextObj fSharedContext;
+#elif defined(SK_BUILD_FOR_ANDROID)
+    EGLContext fContext;
+    EGLDisplay fDisplay;
+    EGLSurface fSurface;
+    EGLContext fSharedContext;
+#elif defined(SK_BUILD_FOR_UNIX)
+    GLXContext fContext;
+    Display* fDisplay;
+    Pixmap fPixmap;
+    GLXPixmap fGlxPixmap;
+    GLXContext fSharedContext;
+#else
+#error "No shared contexts on this platform."
+#endif
+
+    GrGLExtensions fExtensions;
+    GrGLuint fFBO;
+    GrGLuint fTextureID;
+    GrGLuint fColorBufferID;
+    GrGLuint fDepthStencilBufferID;
+
+    const GrGLInterface* fGL;
+    GrContext* fGrContext;
+
+    typedef SkRefCnt INHERITED;
+};
+
+#endif
---
+++ src/gpu/gl/mac/SkNativeSharedGLContext_mac.cpp
@@ -0,0 +1,197 @@
+
+/*
+ * Copyright 2013 The Servo Project Developers
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "gl/SkNativeSharedGLContext.h"
+#include "gl/GrGLUtil.h"
+
+// This is defined here instead of in GrGLDefines.h, to make it easier
+// to rebase our Skia changes onto later versions.
+#define GR_GL_TEXTURE_RECTANGLE_ARB                         0x84F5
+
+SkNativeSharedGLContext::SkNativeSharedGLContext(GrGLSharedContext sharedContext)
+    : fContext(NULL)
+    , fGrContext(NULL)
+    , fGL(NULL)
+    , fSharedContext(sharedContext)
+    , fFBO(0)
+    , fColorBufferID(0)
+    , fDepthStencilBufferID(0) {
+}
+
+SkNativeSharedGLContext::~SkNativeSharedGLContext() {
+    this->destroyGLContext();
+    if (fGrContext) {
+        fGrContext->Release();
+    }
+}
+
+void SkNativeSharedGLContext::destroyGLContext() {
+    if (NULL != fContext) {
+        CGLReleaseContext(fContext);
+    }
+}
+
+const GrGLInterface* SkNativeSharedGLContext::createGLContext() {
+    SkASSERT(NULL == fContext);
+
+    CGLPixelFormatObj pixFormat = CGLGetPixelFormat(fSharedContext);
+
+    if (NULL == pixFormat) {
+        SkDebugf("CGLGetPixelFormat failed.");
+        return NULL;
+    }
+
+    CGLError err = CGLCreateContext(pixFormat, fSharedContext, &fContext);
+
+    if (NULL == fContext) {
+        SkDebugf("CGLCreateContext failed with %s.", CGLErrorString(err));
+        return NULL;
+    }
+
+    CGLSetCurrentContext(fContext);
+
+    const GrGLInterface* interface = GrGLCreateNativeInterface();
+    if (NULL == interface) {
+        SkDebugf("Context could not create GL interface.\n");
+        this->destroyGLContext();
+        return NULL;
+    }
+
+    return interface;
+}
+
+bool SkNativeSharedGLContext::init(const int width, const int height) {
+    if (fGL) {
+        fGL->unref();
+        this->destroyGLContext();
+    }
+
+    fGL = this->createGLContext();
+    if (fGL) {
+        const GrGLubyte* temp;
+
+        GrGLBinding bindingInUse = GrGLGetBindingInUse(this->gl());
+
+        if (!fGL->validate(bindingInUse) || !fExtensions.init(bindingInUse, fGL)) {
+            fGL = NULL;
+            this->destroyGLContext();
+            return false;
+        }
+
+        SK_GL_RET(*this, temp, GetString(GR_GL_VERSION));
+        const char* versionStr = reinterpret_cast<const char*>(temp);
+        GrGLVersion version = GrGLGetVersionFromString(versionStr);
+
+        // clear any existing GL erorrs
+        GrGLenum error;
+        do {
+            SK_GL_RET(*this, error, GetError());
+        } while (GR_GL_NO_ERROR != error);
+
+        SK_GL(*this, GenFramebuffers(1, &fFBO));
+        SK_GL(*this, BindFramebuffer(GR_GL_FRAMEBUFFER, fFBO));
+        SK_GL(*this, GenTextures(1, &fTextureID));
+        SK_GL(*this, BindTexture(GR_GL_TEXTURE_2D, fTextureID));
+        SK_GL(*this, TexImage2D(GR_GL_TEXTURE_2D, 0,
+                                GR_GL_RGBA,
+                                width, height, 0,
+                                GR_GL_RGBA, GR_GL_UNSIGNED_BYTE, 
+                                NULL));
+        SK_GL(*this, TexParameteri(GR_GL_TEXTURE_2D, GR_GL_TEXTURE_WRAP_S, GR_GL_CLAMP_TO_EDGE));
+        SK_GL(*this, TexParameteri(GR_GL_TEXTURE_2D, GR_GL_TEXTURE_WRAP_T, GR_GL_CLAMP_TO_EDGE));
+        SK_GL(*this, TexParameteri(GR_GL_TEXTURE_2D, GR_GL_TEXTURE_MAG_FILTER, GR_GL_NEAREST));
+        SK_GL(*this, TexParameteri(GR_GL_TEXTURE_2D, GR_GL_TEXTURE_MIN_FILTER, GR_GL_NEAREST));
+        SK_GL(*this, FramebufferTexture2D(GR_GL_FRAMEBUFFER,
+                                          GR_GL_COLOR_ATTACHMENT0,
+                                          GR_GL_TEXTURE_2D,
+                                          fTextureID, 0));
+        SK_GL(*this, GenRenderbuffers(1, &fDepthStencilBufferID));
+        SK_GL(*this, BindRenderbuffer(GR_GL_RENDERBUFFER, fDepthStencilBufferID));
+
+        // Some drivers that support packed depth stencil will only succeed
+        // in binding a packed format an FBO. However, we can't rely on packed
+        // depth stencil being available.
+        bool supportsPackedDepthStencil;
+        if (kES2_GrGLBinding == bindingInUse) {
+            supportsPackedDepthStencil = this->hasExtension("GL_OES_packed_depth_stencil");
+        } else {
+            supportsPackedDepthStencil = version >= GR_GL_VER(3,0) ||
+                                         this->hasExtension("GL_EXT_packed_depth_stencil") ||
+                                         this->hasExtension("GL_ARB_framebuffer_object");
+        }
+
+        if (supportsPackedDepthStencil) {
+            // ES2 requires sized internal formats for RenderbufferStorage
+            // On Desktop we let the driver decide.
+            GrGLenum format = kES2_GrGLBinding == bindingInUse ?
+                                    GR_GL_DEPTH24_STENCIL8 :
+                                    GR_GL_DEPTH_STENCIL;
+            SK_GL(*this, RenderbufferStorage(GR_GL_RENDERBUFFER,
+                                             format,
+                                             width, height));
+            SK_GL(*this, FramebufferRenderbuffer(GR_GL_FRAMEBUFFER,
+                                                 GR_GL_DEPTH_ATTACHMENT,
+                                                 GR_GL_RENDERBUFFER,
+                                                 fDepthStencilBufferID));
+        } else {
+            GrGLenum format = kES2_GrGLBinding == bindingInUse ?
+                                    GR_GL_STENCIL_INDEX8 :
+                                    GR_GL_STENCIL_INDEX;
+            SK_GL(*this, RenderbufferStorage(GR_GL_RENDERBUFFER,
+                                             format,
+                                             width, height));
+        }
+        SK_GL(*this, FramebufferRenderbuffer(GR_GL_FRAMEBUFFER,
+                                             GR_GL_STENCIL_ATTACHMENT,
+                                             GR_GL_RENDERBUFFER,
+                                             fDepthStencilBufferID));
+        SK_GL(*this, Viewport(0, 0, width, height));
+        SK_GL(*this, ClearStencil(0));
+        SK_GL(*this, Clear(GR_GL_STENCIL_BUFFER_BIT));
+
+        SK_GL_RET(*this, error, GetError());
+        GrGLenum status;
+        SK_GL_RET(*this, status, CheckFramebufferStatus(GR_GL_FRAMEBUFFER));
+
+        if (GR_GL_FRAMEBUFFER_COMPLETE != status ||
+            GR_GL_NO_ERROR != error) {
+            fFBO = 0;
+            fColorBufferID = 0;
+            fDepthStencilBufferID = 0;
+            fGL->unref();
+            fGL = NULL;
+            this->destroyGLContext();
+            return false;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
+
+GrContext *SkNativeSharedGLContext::getGrContext() {
+    if (fGrContext) {
+        return fGrContext;
+    } else {
+        GrBackendContext p3dctx = reinterpret_cast<GrBackendContext>(this->gl());
+        fGrContext = GrContext::Create(kOpenGL_GrBackend, p3dctx);
+        if (fGrContext == NULL) {
+            return NULL;
+        }
+        fGrContext->AddRef();
+        return fGrContext;
+    }
+}
+
+void SkNativeSharedGLContext::makeCurrent() const {
+    CGLSetCurrentContext(fContext);
+}
+
+void SkNativeSharedGLContext::flush() const {
+    this->makeCurrent();
+    SK_GL(*this, Flush());
+}
