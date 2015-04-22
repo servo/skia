d60e6753b2b23b3b4ff787b505da56cfad0335c9
Simon Sapin <simon.sapin@exyr.org>
2015-04-23 00:33:41 +0200

    Upgrade to rustc 1.0.0-dev (47551b574 2015-04-14) (built 2015-03-18)

--- build.rs
+++ build.rs
@@ -2,20 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![feature(io, os)]
-
-use std::old_io::process::{Command, ProcessExit, StdioContainer};
-use std::os;
+use std::process::Command;
+use std::env;
 
 
 fn main() {
-    let out_dir = os::getenv("OUT_DIR").unwrap();
-    let result = Command::new("make")
+    assert!(Command::new("make")
         .args(&["-f", "makefile.cargo", "-j4"])
-        .stdout(StdioContainer::InheritFd(1))
-        .stderr(StdioContainer::InheritFd(2))
         .status()
-        .unwrap();
-    assert_eq!(result, ProcessExit::ExitStatus(0));
-    println!("cargo:rustc-flags=-L native={}", out_dir);
+        .unwrap()
+        .success());
+    println!("cargo:rustc-flags=-L native={}", env::var("OUT_DIR").unwrap());
 }
