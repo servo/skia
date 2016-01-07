ebc333419bfe4332cabd77a04ae753964f838447
Richard Diamond <wichard@vitalitystudios.com>
2016-01-07 15:05:51 -0600

    Add new FFI functions for the new functions in skia-c.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,12 @@
 
 extern crate libc;
 
+#[cfg(any(target_os="linux", target_os="android", target_os="windows"))]
+extern crate freetype_sys;
+
+#[cfg(any(target_os="linux", target_os="android", target_os="windows"))]
+extern crate fontconfig_sys;
+
 use libc::*;
 
 pub type SkiaGrContextRef = *mut c_void;
@@ -34,6 +40,7 @@ pub type Image     = *mut c_void;
 pub type Data      = *mut c_void;
 pub type Path      = *mut c_void;
 pub type Paint     = *mut c_void;
+pub type Typeface  = *mut c_void;
 pub type GrContext = *mut c_void;
 
 pub type Color = libc::uint32_t;
@@ -51,9 +58,9 @@ pub enum ColorType {
     Gray8 = 7,
 }
 
-#[cfg(target_endian = "little")]
-pub const NATIVE_COLOR_TYPE: ColorType = ColorType::Rgba8888;
 #[cfg(target_endian = "big")]
+pub const NATIVE_COLOR_TYPE: ColorType = ColorType::Rgba8888;
+#[cfg(target_endian = "little")]
 pub const NATIVE_COLOR_TYPE: ColorType = ColorType::Bgra8888;
 
 impl Default for ColorType {
@@ -123,6 +130,37 @@ pub enum CacheManagement {
 }
 
 #[repr(C)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum TypefaceStyle {
+    Normal = 0x00,
+    Bold = 0x01,
+    Italic = 0x02,
+    BoldItalic = 0x03,
+}
+impl Default for TypefaceStyle {
+    fn default() -> TypefaceStyle {
+        TypefaceStyle::Normal
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum Align {
+    Left,
+    Center,
+    Right,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum TextEncoding {
+    Utf8,
+    Utf16,
+    Utf32,
+    GlyphId,
+}
+
+#[repr(C)]
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct ImageInfo {
     pub width: libc::c_int,
@@ -145,6 +183,16 @@ pub struct Rect {
     pub right: libc::c_float,
     pub bottom: libc::c_float,
 }
+impl Default for Rect {
+    fn default() -> Rect {
+        Rect {
+            left: 0.0f32,
+            top: 0.0f32,
+            right: 0.0f32,
+            bottom: 0.0f32,
+        }
+    }
+}
 #[repr(C)]
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct IRect {
@@ -153,6 +201,16 @@ pub struct IRect {
     pub right: libc::int32_t,
     pub bottom: libc::int32_t,
 }
+impl Default for IRect {
+    fn default() -> IRect {
+        IRect {
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+        }
+    }
+}
 
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq, Debug)]
@@ -160,11 +218,27 @@ pub struct Size {
     pub width: libc::c_float,
     pub height: libc::c_float,
 }
+impl Default for Size {
+    fn default() -> Size {
+        Size {
+            width: 0.0f32,
+            height: 0.0f32,
+        }
+    }
+}
 #[repr(C)]
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct ISize {
     pub width: libc::int32_t,
     pub height: libc::int32_t
+}
+impl Default for ISize {
+    fn default() -> ISize {
+        ISize {
+            width: 0,
+            height: 0,
+        }
+    }
 }
 
 #[repr(C)]
@@ -172,6 +246,28 @@ pub struct ISize {
 pub struct Point {
     pub x: libc::c_float,
     pub y: libc::c_float,
+}
+impl Default for Point {
+    fn default() -> Point {
+        Point {
+            x: 0.0f32,
+            y: 0.0f32,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum GrContextFlushFlags {
+    None = 0,
+    Discard = 0x2,
+}
+#[repr(C)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum PaintStyle {
+    Fill,
+    Stroke,
+    StrokeAndFill,
 }
 
 extern {
@@ -191,21 +287,55 @@ extern {
     pub fn sk_data_unref(d: Data) -> Error;
     pub fn sk_paint_ref(p: Paint) -> Error;
     pub fn sk_paint_unref(p: Paint) -> Error;
+    pub fn sk_typeface_ref(p: Typeface);
+    pub fn sk_typeface_unref(p: Typeface);
 
     pub fn sk_image_get_size(i: Image, size: *mut ISize) -> Error;
+    pub fn sk_image_get_gr_backing_handle(i: Image) -> libc::intptr_t;
+
+    pub fn sk_typeface_create_from_name(family_name: *const libc::c_char,
+                                        family_name_len: libc::size_t,
+                                        style: TypefaceStyle) -> Typeface;
+    pub fn sk_typeface_create_from_typeface(family: Typeface,
+                                            style: TypefaceStyle) -> Typeface;
+    pub fn sk_typeface_create_from_path(path: *const libc::c_char,
+                                        path_len: libc::size_t) -> Typeface;
 
     pub fn sk_new_paint() -> Paint;
     pub fn sk_new_paint_copy(p: Paint) -> Paint;
     pub fn sk_paint_reset(p: Paint) -> Error;
     pub fn sk_paint_get_color(p: Paint) -> Color;
     pub fn sk_paint_set_color(p: Paint, c: Color);
+    pub fn sk_paint_get_typeface(p: Paint) -> Typeface;
+    pub fn sk_paint_set_typeface(p: Paint, tf: Typeface);
+    pub fn sk_paint_get_anti_alias(p: Paint) -> bool;
+    pub fn sk_paint_set_anti_alias(p: Paint, v: bool);
+    pub fn sk_paint_get_subpixel_text(p: Paint) -> bool;
+    pub fn sk_paint_set_subpixel_text(p: Paint, v: bool);
+    pub fn sk_paint_get_lcd_render_text(p: Paint) -> bool;
+    pub fn sk_paint_set_lcd_render_text(p: Paint, v: bool);
+    pub fn sk_paint_get_text_size(p: Paint) -> libc::c_float;
+    pub fn sk_paint_set_text_size(p: Paint, size: libc::c_float);
+    pub fn sk_paint_get_text_x_scale(p: Paint) -> libc::c_float;
+    pub fn sk_paint_set_text_x_scale(p: Paint, scale: libc::c_float);
+    pub fn sk_paint_get_text_align(p: Paint) -> Align;
+    pub fn sk_paint_set_text_align(p: Paint, align: Align);
+    pub fn sk_paint_get_text_encoding(p: Paint) -> TextEncoding;
+    pub fn sk_paint_set_text_encoding(p: Paint, encoding: TextEncoding);
+    pub fn sk_paint_get_style(p: Paint) -> PaintStyle;
+    pub fn sk_paint_set_style(p: Paint, s: PaintStyle);
+    pub fn sk_paint_measure_text(p: Paint, text: *const libc::c_void, length: libc::size_t,
+                                 bounds: *mut Rect, scale: libc::c_float) -> libc::c_float;
 
     pub fn sk_new_render_target_surface(gr: GrContext,
                                         budget: CacheManagement,
                                         info: ImageInfo) -> Surface;
+    pub fn sk_new_raster_direct_surface(info: ImageInfo, dest: *mut libc::c_void,
+                                        row_bytes: libc::size_t) -> Surface;
 
     pub fn sk_new_image_snapshot(s: Surface) -> Image;
     pub fn sk_surface_get_size(s: Surface, size: *mut ISize) -> Error;
+    pub fn sk_surface_get_image_info(s: Surface) -> ImageInfo;
 
     pub fn sk_flush(s: Surface) -> Error;
     pub fn sk_save(s: Surface, save_count: *mut libc::c_int) -> Error;
@@ -216,8 +346,12 @@ extern {
     pub fn sk_scale(s: Surface, sx: libc::c_float, sy: libc::c_float) -> Error;
     pub fn sk_rotate(s: Surface, degrees: libc::c_float) -> Error;
 
+    pub fn sk_surface_discard(s: Surface);
+    pub fn sk_surface_save_layer_alpha(s: Surface, bounds: *const Rect, alpha: u8) -> libc::c_int;
+
     pub fn sk_clip_rect(s: Surface, rect: Rect) -> Error;
     pub fn sk_draw_paint(s: Surface, paint: Paint) -> Error;
+    pub fn sk_surface_draw_line(s: Surface, paint: Paint, start: Point, end: Point);
     pub fn sk_draw_points(s: Surface, paint: Paint, mode: PointMode,
                           points: *const Point, count: libc::size_t) -> Error;
     pub fn sk_draw_path(s: Surface, paint: Paint, path: Path) -> Error;
@@ -232,10 +366,13 @@ extern {
     pub fn sk_path_reset(p: Path) -> Error;
     pub fn sk_path_set_fill_type(p: Path, ft: PathFillType) -> Error;
     pub fn sk_path_get_fill_type(p: Path) -> PathFillType;
+    pub fn sk_path_move_to(p: Path, to: Point, relative: bool);
     pub fn sk_path_line_to(p: Path, point: Point, relative: bool) -> Error;
     pub fn sk_path_quad_to(p: Path, p1: Point, p2: Point, relative: bool) -> Error;
     pub fn sk_path_cubic_to(p: Path, p1: Point, p2: Point, p3: Point, relative: bool) -> Error;
     pub fn sk_path_close(p: Path);
     pub fn sk_path_count_points(p: Path) -> libc::c_int;
     pub fn sk_path_get_point(p: Path, index: libc::c_int) -> Point;
+
+    pub fn gr_context_flush(context: GrContext, flags: GrContextFlushFlags);
 }
