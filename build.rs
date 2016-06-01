f7b85adab5ad56deb8da2b33a8880db9e314f511
Vladimir Vukicevic <vladimir@pobox.com>
2016-05-31 20:31:52 -0600

    Switch build to CMake, support MSVC

--- build.rs
+++ build.rs
@@ -2,33 +2,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use std::process::Command;
+extern crate cmake;
+
 use std::env;
 
-
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    assert!(Command::new("make")
-        .args(&["-R", "-f", "makefile.cargo", &format!("-j{}", env::var("NUM_JOBS").unwrap())])
-        .status()
-        .unwrap()
-        .success());
-    println!("cargo:rustc-link-search=native={}", out_dir);
+    let dst = cmake::Config::new(".")
+        .define("CMAKE_C_COMPILER", "cl.exe")
+        .define("CMAKE_CXX_COMPILER", "cl.exe")
+        .define("CMAKE_LINKER", "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\link.exe")
+        .build();
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static=skia");
-    println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:outdir={}", out_dir);
+    println!("cargo:outdir={}", dst.display());
 
     let target = env::var("TARGET").unwrap();
     if target.contains("unknown-linux-gnu") {
+        println!("cargo:rustc-link-lib=stdc++");
         println!("cargo:rustc-link-lib=bz2");
         println!("cargo:rustc-link-lib=GL");
     } else if target.contains("eabi") {
+        println!("cargo:rustc-link-lib=stdc++");
         println!("cargo:rustc-link-lib=GLESv2");
     } else if target.contains("apple-darwin") {
+        println!("cargo:rustc-link-lib=stdc++");
         println!("cargo:rustc-link-lib=framework=OpenGL");
         println!("cargo:rustc-link-lib=framework=ApplicationServices");
     } else if target.contains("windows") {
+        if target.contains("gnu") {
+            println!("cargo:rustc-link-lib=stdc++");
+        }
         println!("cargo:rustc-link-lib=usp10");
         println!("cargo:rustc-link-lib=ole32");
     }
